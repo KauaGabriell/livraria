d73654209de33616ef9923b298241280c7e18781
Kauã Gabriell <kauagabriell64@gmail.com>
2025-10-17 16:13:30 -0300

    :sparkles: feat: Adicionando lógica de verificar se o usuário já existe.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,8 @@ export async function userCreate(req, res) {
     const user = new User(req.body);
     await user.register();
 
-    res.status(201).json(user.body);
+     
+    res.status(201).json({name: user.user.name, email: user.user.email});
   } catch (error) 
   {
    return res.status(501).json({message: 'Não foi possível cadastrar seu usuário.'})
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,36 +1,46 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import bcrypt, { genSaltSync } from 'bcrypt';
 
 //Criando Schema do Model
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true}
-})
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true, select: false },
+});
 
 //Model do Mongoose
 const userModel = mongoose.model('User', userSchema);
 
 //Classe User
 class User {
-    constructor(body){
-        this.body = body;
-        this.errors = [];
-        this.user = null;
-    }
+  constructor(body) {
+    this.body = body;
+    this.errors = [];
+    this.user = null;
+  }
 
-    //Método para cadastrar/criar um usuário.
-    async register(){
-        //Adicionando um hash na senha com bcrypt
-        const salt = genSaltSync();
-        this.body.password = await bcrypt.hash(this.body.password, salt);
+  //Método para cadastrar/criar um usuário.
+  async register() {
+    await this.userExist();  //Verifica se o usuário já existe.
 
-        
-        this.user = await userModel.create(this.body);
+    if(this.errors.length > 0) return //Verifica se tem alguma erro na nossa criação, se tiver para a aplicação.
+
+    //Adicionando um hash na senha com bcrypt
+    const salt = genSaltSync();
+    this.body.password = await bcrypt.hash(this.body.password, salt);
+
+    this.user = await userModel.create(this.body); //Cria o usuário.
+  }
+
+  //Método para verificar se o usuário existe.
+  async userExist() {
+    const user = await userModel.findOne({email: this.body.email});
+    if(user){
+        this.errors.push("Usuário Já Cadastrado");
     }
+  }
+
+  
 }
 
 export default User;
-
-
-
