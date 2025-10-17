18915918e2c046fbba99ee97eb9efa832014eb37
Kauã Gabriell <kauagabriell64@gmail.com>
2025-10-17 10:32:36 -0300

    :package: feat: Adicionando bcrypt e adicionando hash na senha para salvar no banco de dados.

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
+import bcrypt, { genSaltSync } from 'bcrypt';
 
+//Criando Schema do Model
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true}
 })
 
+//Model do Mongoose
 const userModel = mongoose.model('User', userSchema);
 
+//Classe User
 class User {
     constructor(body){
         this.body = body;
@@ -15,7 +19,13 @@ class User {
         this.user = null;
     }
 
+    //Método para cadastrar/criar um usuário.
     async register(){
+        //Adicionando um hash na senha com bcrypt
+        const salt = genSaltSync();
+        this.body.password = await bcrypt.hash(this.body.password, salt);
+
+        
         this.user = await userModel.create(this.body);
     }
 }
